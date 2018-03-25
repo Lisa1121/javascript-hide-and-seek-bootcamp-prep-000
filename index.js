2c9cf9ed266d3847a1b52e23e47e2e53eace79e9
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-25 17:15:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function getFirstSelector (selector)  {
+  return document.querySelector (selector)
+}
+function nestedTarget () {
+  return document.querySelector ('#nested .target')
+}
+function increaseRankBy(n) {
+  let list= document.querySelectorAll('ul.ranked-list li')
+  for (let i=0; i<list.length; i++) {
+    list[i].innerHTML=parseInt(list[i].innerHTML)+n 
+  }
+}
+function deepestChild() {
+  let list = document.querySelectorAll('#grand-node div')
+  let listArray= Array.from(list)
+  return listArray[listArray.length-1]
+}
