e0abf1eb2832d6cc193e86ac7a6fae137100eaf3
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-25 17:07:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ function nestedTarget () {
   return document.querySelector ('#nested .target')
 }
 function increaseRankBy(n) {
-  let list= document.querySelectorAll('ul.ranked')
+  let list= document.querySelectorAll('ul.ranked-list li')
+  for (let i=0; i<list.length; i++) {
+    list[i].innerHTML=parseInt(list[i].innerHTML)+n 
+  }
 }
 function deepestChild() {
   
